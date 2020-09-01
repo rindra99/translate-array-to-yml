e329ea493e5593374881d6498430da580495e039
Rindra <rraininoro@gmail.com>
2020-09-01 20:16:33 +0400

    fix yml indentation

--- function-translate-array-into-yml.php
+++ function-translate-array-into-yml.php
@@ -9,15 +9,15 @@ function translateArray(array $array, $tab = "")
     static $depth = 0;
     $str = "";
     foreach ($array as $key => $value) {
-        $depth++;
         if (is_array($value)) {
+            $depth++;
             $str .= str_repeat($tab, $depth) . "$key: <br>";
             $str .= $this->translateArray($value, '&nbsp;&nbsp;&nbsp;&nbsp;');
+            $depth--;
         } else {
             $value = addcslashes($value, '"');
             $str .= str_repeat($tab, $depth) . lcfirst(htmlspecialchars(str_replace(' ', '', ucwords($key)) . ': "' . $value) . '"<br>');
         }
-        $depth--;
     }
     return $str;
 }
